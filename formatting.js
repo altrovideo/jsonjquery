e737f7de4e2d40910b74adc7c1a0ae6298d387b3
altrovideo <altrovideo@gmail.com>
2017-05-23 11:10:13 +0200

    first commit

---
+++ formatting.js
@@ -0,0 +1,41 @@
+//<![CDATA[
+    $(window).on("load", function (){
+        jQuery(function($) {
+            $.getJSON('http://public-api.wordpress.com/rest/v1.1/sites/cristianozanca.wordpress.com/posts/?category=woocommerce')
+                .done(function(response) {
+
+                    var i = 0, dataSize = response.posts.length, html = '';
+
+
+
+                    for(i; i < dataSize; i++){
+
+                        html +="<h2>"+response.posts[i].title+"</h2>"+"<p>"+response.posts[i].content+"</p>";
+
+
+                    }
+                    $("div").html(html);
+
+                    /*
+                     var $content = $('#content')
+                     .html(response.posts[2].content);
+
+
+
+                     $content.find('img').each(function() {
+                     var $this = $(this);
+                     $this.height($this.height() / 3);
+                     $this.width($this.width() / 3);
+                     });
+
+                     $('#title')
+                     .html(response.posts[2].title)
+                     .attr('href', response.posts[2].URL);
+                     */
+
+
+                });
+        });
+    });//]]>
+
+
